5922959659a17cb5c633274d81da559076e3602d
dufmel <114317218+dufmel@users.noreply.github.com>
2022-12-15 13:13:02 -0600

    Update pattern-three-js.js
    third finger sharps corrected

--- pattern-three-js.js
+++ pattern-three-js.js
@@ -1,177 +1,177 @@
 //C String Notes: Finger Pattern 3
-document.getElementById("first-finger-c-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("first-finger-c-two").value
+document.getElementById("first-finger-c-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("first-finger-c-three").value
 
    if (pitch === "D" || pitch === "d"){
-      document.getElementById("first-finger-c-two").style.backgroundColor="#00FF00"}
+      document.getElementById("first-finger-c-three").style.backgroundColor="#00FF00"}
       else{
-         document.getElementById("first-finger-c-two").style.backgroundColor="#FF0000"}
+         document.getElementById("first-finger-c-three").style.backgroundColor="#FF0000"}
       })
 
-document.getElementById("second-finger-c-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("second-finger-c-two").value
-      
+document.getElementById("second-finger-c-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("second-finger-c-three").value
+
    if (pitch === "E" || pitch === "e"){
-       document.getElementById("second-finger-c-two").style.backgroundColor="#00FF00"}
-         else{document.getElementById("second-finger-c-two").style.backgroundColor="#FF0000"}
+       document.getElementById("second-finger-c-three").style.backgroundColor="#00FF00"}
+         else{document.getElementById("second-finger-c-three").style.backgroundColor="#FF0000"}
             })
 
-document.getElementById("third-finger-c-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("third-finger-c-two").value
-   
+document.getElementById("third-finger-c-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("third-finger-c-three").value
+
    if (pitch === "F#" || pitch === "f#" || pitch === "F #" || pitch === "f #"){
-   document.getElementById("third-fingercg-two").style.backgroundColor="#00FF00"}
-  else{document.getElementById("third-finger-c-two").style.backgroundColor="#FF0000"}
+   document.getElementById("third-finger-g-three").style.backgroundColor="#00FF00"}
+  else{document.getElementById("third-finger-c-three").style.backgroundColor="#FF0000"}
    })
-   
-document.getElementById("fourth-finger-c-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("fourth-finger-c-two").value
-      
+
+document.getElementById("fourth-finger-c-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("fourth-finger-c-three").value
+
    if (pitch === "G" || pitch === "g"){
-   document.getElementById("fourth-finger-c-two").style.backgroundColor="#00FF00"}
-   else{document.getElementById("fourth-finger-c-two").style.backgroundColor="#FF0000"}
+   document.getElementById("fourth-finger-c-three").style.backgroundColor="#00FF00"}
+   else{document.getElementById("fourth-finger-c-three").style.backgroundColor="#FF0000"}
    })           
 
 //G String Notes Finger Pattern 3
 
-document.getElementById("first-finger-g-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("first-finger-g-two").value
+document.getElementById("first-finger-g-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("first-finger-g-three").value
 
    if (pitch === "A" || pitch === "a"){
-      document.getElementById("first-finger-g-two").style.backgroundColor="#00FF00"}
+      document.getElementById("first-finger-g-three").style.backgroundColor="#00FF00"}
       else{
-         document.getElementById("first-finger-g-two").style.backgroundColor="#FF0000"}
+         document.getElementById("first-finger-g-three").style.backgroundColor="#FF0000"}
       })
 
-document.getElementById("second-finger-g-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("second-finger-g-two").value
-      
+document.getElementById("second-finger-g-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("second-finger-g-three").value
+
    if (pitch === "B" || pitch === "b"){
-       document.getElementById("second-finger-g-two").style.backgroundColor="#00FF00"}
-         else{document.getElementById("second-finger-g-two").style.backgroundColor="#FF0000"}
+       document.getElementById("second-finger-g-three").style.backgroundColor="#00FF00"}
+         else{document.getElementById("second-finger-g-three").style.backgroundColor="#FF0000"}
             })
 
-document.getElementById("third-finger-g-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("third-finger-g-two").value
-   
+document.getElementById("third-finger-g-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("third-finger-g-three").value
+
    if (pitch === "C#" || pitch === "c#" || pitch === "C #" || pitch === "c #"){
-   document.getElementById("third-finger-g-two").style.backgroundColor="#00FF00"}
-  else{document.getElementById("third-finger-g-two").style.backgroundColor="#FF0000"}
+   document.getElementById("third-finger-g-three").style.backgroundColor="#00FF00"}
+  else{document.getElementById("third-finger-g-three").style.backgroundColor="#FF0000"}
    })
-   
-document.getElementById("fourth-finger-g-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("fourth-finger-g-two").value
-      
+
+document.getElementById("fourth-finger-g-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("fourth-finger-g-three").value
+
    if (pitch === "D" || pitch === "d"){
-   document.getElementById("fourth-finger-g-two").style.backgroundColor="#00FF00"}
-   else{document.getElementById("fourth-finger-g-two").style.backgroundColor="#FF0000"}
+   document.getElementById("fourth-finger-g-three").style.backgroundColor="#00FF00"}
+   else{document.getElementById("fourth-finger-g-three").style.backgroundColor="#FF0000"}
    })           
 
 //D String Notes Finger Pattern 3
 
-document.getElementById("first-finger-d-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("first-finger-d-two").value
+document.getElementById("first-finger-d-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("first-finger-d-three").value
 
    if (pitch === "E" || pitch === "e"){
-      document.getElementById("first-finger-d-two").style.backgroundColor="#00FF00"}
+      document.getElementById("first-finger-d-three").style.backgroundColor="#00FF00"}
       else{
-         document.getElementById("first-finger-d-two").style.backgroundColor="#FF0000"}
+         document.getElementById("first-finger-d-three").style.backgroundColor="#FF0000"}
       })
 
-document.getElementById("second-finger-d-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("second-finger-d-two").value
-      
+document.getElementById("second-finger-d-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("second-finger-d-three").value
+
    if (pitch === "F#" || pitch === "f#" || pitch === "F #" || pitch === "f #"){
-       document.getElementById("second-finger-d-two").style.backgroundColor="#00FF00"}
-         else{document.getElementById("second-finger-d-two").style.backgroundColor="#FF0000"}
+       document.getElementById("second-finger-d-three").style.backgroundColor="#00FF00"}
+         else{document.getElementById("second-finger-d-three").style.backgroundColor="#FF0000"}
             })
 
-document.getElementById("third-finger-d-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("third-finger-d-two").value
-   
-   if (pitch === "G#" || pitch === "g#" || pitch === "G #" || pitch === "g #"){
-   document.getElementById("third-finger-d-two").style.backgroundColor="#00FF00"}
-  else{document.getElementById("third-finger-d-two").style.backgroundColor="#FF0000"}
+document.getElementById("third-finger-d-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("third-finger-d-three").value
+
+   if (pitch === "G#" || pitch === "g#" || pitch == "G #" || pitch === "g #"){
+   document.getElementById("third-finger-d-three").style.backgroundColor="#00FF00"}
+  else{document.getElementById("third-finger-d-three").style.backgroundColor="#FF0000"}
    })
-   
-document.getElementById("fourth-finger-d-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("fourth-finger-d-two").value
-      
+
+document.getElementById("fourth-finger-d-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("fourth-finger-d-three").value
+
    if (pitch === "A" || pitch === "a"){
-   document.getElementById("fourth-finger-d-two").style.backgroundColor="#00FF00"}
-   else{document.getElementById("fourth-finger-d-two").style.backgroundColor="#FF0000"}
+   document.getElementById("fourth-finger-d-three").style.backgroundColor="#00FF00"}
+   else{document.getElementById("fourth-finger-d-three").style.backgroundColor="#FF0000"}
    }) 
 
 //A String Notes Finger Pattern 3
 
-document.getElementById("first-finger-a-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("first-finger-a-two").value
+document.getElementById("first-finger-a-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("first-finger-a-three").value
 
    if (pitch === "B" || pitch === "b"){
-      document.getElementById("first-finger-a-two").style.backgroundColor="#00FF00"}
+      document.getElementById("first-finger-a-three").style.backgroundColor="#00FF00"}
       else{
-         document.getElementById("first-finger-a-two").style.backgroundColor="#FF0000"}
+         document.getElementById("first-finger-a-three").style.backgroundColor="#FF0000"}
       })
 
-document.getElementById("second-finger-a-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("second-finger-a-two").value
-      
+document.getElementById("second-finger-a-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("second-finger-a-three").value
+
    if (pitch === "C#" || pitch === "c#" || pitch === "C #" || pitch === "c #"){
-       document.getElementById("second-finger-a-two").style.backgroundColor="#00FF00"}
-         else{document.getElementById("second-finger-a-two").style.backgroundColor="#FF0000"}
+       document.getElementById("second-finger-a-three").style.backgroundColor="#00FF00"}
+         else{document.getElementById("second-finger-a-three").style.backgroundColor="#FF0000"}
             })
 
-document.getElementById("third-finger-a-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("third-finger-a-two").value
-   
+document.getElementById("third-finger-a-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("third-finger-a-three").value
+
    if (pitch === "D#" || pitch === "d#" || pitch === "D #" || pitch === "d #"){
-   document.getElementById("third-finger-a-two").style.backgroundColor="#00FF00"}
-  else{document.getElementById("third-finger-a-two").style.backgroundColor="#FF0000"}
+   document.getElementById("third-finger-a-three").style.backgroundColor="#00FF00"}
+  else{document.getElementById("third-finger-a-three").style.backgroundColor="#FF0000"}
    })
-   
-document.getElementById("fourth-finger-a-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("fourth-finger-a-two").value
-      
+
+document.getElementById("fourth-finger-a-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("fourth-finger-a-three").value
+
    if (pitch === "E" || pitch === "e"){
-   document.getElementById("fourth-finger-a-two").style.backgroundColor="#00FF00"}
-   else{document.getElementById("fourth-finger-a-two").style.backgroundColor="#FF0000"}
+   document.getElementById("fourth-finger-a-three").style.backgroundColor="#00FF00"}
+   else{document.getElementById("fourth-finger-a-three").style.backgroundColor="#FF0000"}
    })
 
    //E String Notes Finger Pattern 3
 
-document.getElementById("first-finger-e-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("first-finger-e-two").value
+document.getElementById("first-finger-e-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("first-finger-e-three").value
 
    if (pitch === "F#" || pitch === "F #" || pitch === "f#" || pitch === "f #"){
-      document.getElementById("first-finger-e-two").style.backgroundColor="#00FF00"}
+      document.getElementById("first-finger-e-three").style.backgroundColor="#00FF00"}
       else{
-         document.getElementById("first-finger-e-two").style.backgroundColor="#FF0000"}
+         document.getElementById("first-finger-e-three").style.backgroundColor="#FF0000"}
       })
 
-document.getElementById("second-finger-e-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("second-finger-e-two").value
-      
+document.getElementById("second-finger-e-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("second-finger-e-three").value
+
    if (pitch === "G#" || pitch === "g#" || pitch === "G #" || pitch === "g #"){
-       document.getElementById("second-finger-e-two").style.backgroundColor="#00FF00"}
-         else{document.getElementById("second-finger-e-two").style.backgroundColor="#FF0000"}
+       document.getElementById("second-finger-e-three").style.backgroundColor="#00FF00"}
+         else{document.getElementById("second-finger-e-three").style.backgroundColor="#FF0000"}
             })
 
-document.getElementById("third-finger-e-two-submit").addEventListener("click", function(){
-   let pitch = document.getElementById("third-finger-e-two").value
-   
+document.getElementById("third-finger-e-three-submit").addEventListener("click", function(){
+   let pitch = document.getElementById("third-finger-e-three").value
+
    if (pitch === "A#" || pitch === "a#" || pitch === "A #" || pitch === "a #"){
-   document.getElementById("third-finger-e-two").style.backgroundColor="#00FF00"}
-  else{document.getElementById("third-finger-e-two").style.backgroundColor="#FF0000"}
+   document.getElementById("third-finger-e-three").style.backgroundColor="#00FF00"}
+  else{document.getElementById("third-finger-e-three").style.backgroundColor="#FF0000"}
    })
-   
-   document.getElementById("fourth-finger-e-two-submit").addEventListener("click", function(){
-      let pitch = document.getElementById("fourth-finger-e-two").value
-         
+
+   document.getElementById("fourth-finger-e-three-submit").addEventListener("click", function(){
+      let pitch = document.getElementById("fourth-finger-e-three").value
+
       if (pitch === "B" || pitch === "b"){
-         document.getElementById("fourth-finger-e-two").style.backgroundColor="#00FF00"}
-      else {document.getElementById("fourth-finger-e-two").style.backgroundColor="#FF0000"}
+         document.getElementById("fourth-finger-e-three").style.backgroundColor="#00FF00"}
+      else {document.getElementById("fourth-finger-e-three").style.backgroundColor="#FF0000"}
       })
-   
+
 //Reset Button
 
 let resetBtn=document.getElementById("reset-btn")
